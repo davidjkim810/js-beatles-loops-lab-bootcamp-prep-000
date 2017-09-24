d4d9a60b3a02d2bf77e5142fcd8f217d21af7917
David Kim <davidjkim810@gmail.com>
2017-09-24 15:24:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments) {
+  var x = [];
+for (let y = 0; y < musicians.length; y++) {
+x.push(`${musicians[y]} plays ${instruments[y]}`)
+} return x
+}
+
+
+function johnLennonFacts(facts){
+  let x = 0;
+  while (x < facts.length) {
+    console.log('${facts[x]}!!!');
+    x++;
+  } return facts
