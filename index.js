1dea5ee8aed15096780de968ae5bfa2e687865c1
David Kim <davidjkim810@gmail.com>
2017-09-24 15:24:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,3 @@ for (let y = 0; y < musicians.length; y++) {
 x.push(`${musicians[y]} plays ${instruments[y]}`)
 } return x
 }
-
-
-function johnLennonFacts(facts){
-  let x = 0;
-  while (x < facts.length) {
-    console.log('${facts[x]}!!!');
-    x++;
-  } return facts
